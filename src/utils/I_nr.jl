9f4930110cc7659a879f5233b280d359b96fd3c8
clwelch03 <catherinewelch@proton.me>
2025-07-10 19:25:07 -0700

    teehee!

--- src/utils/I_nr.jl
+++ src/utils/I_nr.jl
@@ -6,7 +6,7 @@ export I
 inr_table = Vector{Vector{Float32}}()  # Vector of vectors (matrix-like)
 
 # Open the CSV file and process it line by line
-open("src/utils/inr_output_250_250.csv", "r") do file
+open("./inr_output_250_250.csv", "r") do file
 	# Iterate over each line of the file
 	for line in eachline(file)
 		# Split the line by commas (CSV format) and push it as a new vector to the matrix
